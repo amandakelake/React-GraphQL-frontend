08b09542b89156ff24ebb9d2e2f4254ec3f6a78c
皛白 <930007017@qq.com>
2018-11-27 11:31:32 +0800

    14 可以创建item  未关联上user  新增会报错  但可手动创建

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,16 @@
-const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
-  }
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem({
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    console.log(item);
+    return item;
+  },
 };
 
-module.exports = mutations;
+module.exports = Mutations;
